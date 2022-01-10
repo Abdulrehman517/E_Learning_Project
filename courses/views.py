a0fdbed48f56c8a825332c0004a1bd802f4d355a
unknown <abdulrehman517uetian@gmail.com>
2022-01-10 15:32:16 +0500

    testing

--- courses/views.py
+++ courses/views.py
@@ -11,6 +11,8 @@ from .forms import ModuleFormSet
 
 from django.forms.models import modelform_factory
 from django.apps import apps
+
+
 #
 # class ManageCourseListView(ListView):
 #     model = Course
@@ -26,10 +28,12 @@ class OwnerMixin(object):
         qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
 
+
 class OwnerEditMixin(object):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
 
 class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course
@@ -68,20 +72,20 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
     def get_formset(self, data=None):
         return ModuleFormSet(instance=self.course, data=data)
 
-    def dispatch(self,request, pk):
+    def dispatch(self, request, pk):
         self.course = get_object_or_404(Course, id=pk, owner=request.user)
         return super().dispatch(request, pk)
 
     def get(self, request, *args, **kwargs):
         formset = self.get_formset()
-        return self.render_to_response({'course':self.course, 'formset':formset})
+        return self.render_to_response({'course': self.course, 'formset': formset})
 
     def post(self, request, *args, **kwargs):
         formset = self.get_formset(data=request.POST)
         if formset.is_valid():
             formset.save()
             return redirect('manage_course_list')
-        return self.render_to_response({'course':self.course, 'formset':formset})
+        return self.render_to_response({'course': self.course, 'formset': formset})
 
 
 class ContentCreateUpdateView(TemplateResponseMixin, View):
@@ -136,14 +140,13 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
                                        item=obj)
                 return redirect('module_content_list', self.module.id)
             return self.render_to_response({'form': form,
-                                                'object': self.obj})
+                                            'object': self.obj})
 
 
 class ContentDeleteView(View):
     def post(self, request, id):
-
-         content = get_object_or_404(Content,id=id,module__course__owner=request.user)
-         module = content.module
-         content.item.delete()
-         content.delete()
-         return redirect('module_content_list', module.id)
+        content = get_object_or_404(Content, id=id, module__course__owner=request.user)
+        module = content.module
+        content.item.delete()
+        content.delete()
+        return redirect('module_content_list', module.id)
